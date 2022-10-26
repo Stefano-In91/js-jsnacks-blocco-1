c61fadf450f750d6cc98d51ed07f433ec0375228
Stefano.In91 <ste.ince91@gmail.com>
2022-10-26 10:56:53 +0200

    adds j-snack1

---
+++ jsnack1/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const numberOne = prompt("Inserisci numero");
+const numberTwo = prompt("Inserisci numero");
+
+if (numberOne >= numberTwo) {
+   alert(`${numberOne} ${numberTwo}`);
+} else {
+   alert(`${numberTwo} ${numberOne}`)
+}
